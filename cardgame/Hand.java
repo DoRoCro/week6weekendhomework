3211614d7b47046ee58a482fcf2cc2f6f4bd6112
Douglas Crooke <douglas.crooke+codeclan@googlemail.com>
2017-03-11 18:28:15 +0000

    add Hand class

--- cardgame/Hand.java
+++ cardgame/Hand.java
@@ -1,17 +1,20 @@
-// package cardgame;
+package cardgame;
+import java.util.ArrayList;
+import java.util.*;
 
-// public class Hand implements PlayerAttribs {
+public class Hand implements PlayerAttribs {
 
-//   // Hand represents a player's card holding
-//   // cards which can be shown or hidden
-//   private ArrayList<Card> hand;
+  // Hand represents a player's card holding
+  // cards which can be shown or hidden
+  private ArrayList<Card> hand;
+  private Player player;
 
-//   public Hand(Player player){
-//     this.player = player;
-//     hand = new ArrayList<Card>();
-//   }
+  public Hand(Player player){
+    this.player = player;
+    hand = new ArrayList<Card>();
+  }
 
-//   public String getName() {
-//     return this.player().getName();
-//   }
-// }
+  public String getName() {
+    return this.player.getName();
+  }
+}
--- specs/HandTest.java
+++ specs/HandTest.java
@@ -1,22 +1,23 @@
-// import static org.junit.Assert.*;
-// import org.junit.*;
-// import cardgame.*;
+import static org.junit.Assert.*;
+import org.junit.*;
+import cardgame.*;
 
-// public class HandTest {
-//   Player player1;
-//   Player player2;
-//   Hand hand1;
-//   Hand hand2;
+public class HandTest {
+  Player player1;
+  Player player2;
+  Hand hand1;
+  Hand hand2;
 
-//   @Before
-//   public void before(){
-//     player1 = new Player("Lister");
-//     player2 = new Player("Rimmer");
-//     hand1 = new Hand(player1);
-//     hand2 = new Hand(player2);
-//   }
+  @Before
+  public void before(){
+    player1 = new Player("Lister");
+    player2 = new Player("Rimmer");
+    hand1 = new Hand(player1);
+    hand2 = new Hand(player2);
+  }
 
-//   @Test
-//   public void handHasNameFromPlayer() {
-//     assertEquals("Lister", hand1.getName());
-//   }
+  @Test
+  public void handHasNameFromPlayer() {
+    assertEquals("Lister", hand1.getName());
+  }
+}
