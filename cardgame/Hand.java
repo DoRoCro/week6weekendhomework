2e723a741022f65140069b44cf1e04fa59a4ff53
Douglas Crooke <douglas.crooke+codeclan@googlemail.com>
2017-03-11 19:24:51 +0000

    add Hand class method to receive and turn over cards

--- cardgame/Hand.java
+++ cardgame/Hand.java
@@ -6,15 +6,31 @@ public class Hand implements PlayerAttribs {
 
   // Hand represents a player's card holding
   // cards which can be shown or hidden
-  private ArrayList<Card> hand;
+  // cards are received and held FACEDOWN (i.e. hidden) by default
+  // A hand therefore consists of two lists, visible and hidden cards.
+
+  private ArrayList<Card> faceups;
+  private ArrayList<Card> facedowns;
   private Player player;
 
   public Hand(Player player){
     this.player = player;
-    hand = new ArrayList<Card>();
+    this.faceups = new ArrayList<Card>();
+    this.facedowns = new ArrayList<Card>();
   }
 
   public String getName() {
     return this.player.getName();
+  }
+
+  public void receiveCard( Card card ){
+    this.facedowns.add(card);
+  }
+
+  public String showCardByIndex(int index){
+    // move card from facedowns to end of faceups
+    // and return card face details
+    this.faceups.add( this.facedowns.remove(index) );
+    return this.faceups.get(this.faceups.size() - 1 ).toString();
   }
 }
--- specs/HandTest.java
+++ specs/HandTest.java
@@ -7,6 +7,7 @@ public class HandTest {
   Player player2;
   Hand hand1;
   Hand hand2;
+  Card card1;
 
   @Before
   public void before(){
@@ -14,10 +15,18 @@ public class HandTest {
     player2 = new Player("Rimmer");
     hand1 = new Hand(player1);
     hand2 = new Hand(player2);
+    card1 = new Card(Suit.HEARTS, CardNumber.QUEEN);
   }
 
   @Test
   public void handHasNameFromPlayer() {
     assertEquals("Lister", hand1.getName());
+    assertEquals("Rimmer", hand2.getName());
+  }
+
+  @Test
+  public void handReceivesCardFaceDown(){
+    hand1.receiveCard(card1);
+    assertEquals("QUEEN of HEARTS", hand1.showCardByIndex(0));
   }
 }
