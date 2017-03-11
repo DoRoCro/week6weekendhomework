e376c06d89c03dceeb63cfb66ae5f66b7777fa1f
Douglas Crooke <douglas.crooke+codeclan@googlemail.com>
2017-03-11 21:14:53 +0000

    add outline Game class

--- cardgame/GameController.java
+++ cardgame/GameController.java
@@ -1,1 +1,11 @@
 package cardgame;
+public interface GameController{
+  //public void table(int decks, String[] players);
+  //, Viewer viewer, TurnLog turnlog);
+  // public void turn(TurnLog turnlog);   // each player gets a turn in a round
+  // public void round();  // in a round, each player has a turn in order
+  // public Boolean isWon();  // winning condition
+  // public Boolean isOver(); // non-winning end condition
+  // public void viewer();    // MVC viewer
+  // public void turnLog();   // something for the viewer to show...
+}
---
+++ specs/GameTest.java
@@ -0,0 +1,28 @@
+import static org.junit.Assert.*;
+import org.junit.*;
+import cardgame.*;
+
+public class GameTest {
+  Deck deck;
+  Player player1;
+  Player player2;
+  Hand hand1;
+  Hand hand2;
+  Game game;
+  String player1name = "Zaphod";
+  String player2name = "Zarquon";
+  String[] players;
+
+  @Before
+  public void before(){  
+    players = new String[2];
+    players[0] = player1name;
+    players[1] = player2name;
+  }
+
+  @Test
+  public void checkGameHasPlayers(){
+    game = new Game( 1, players );
+    assertEquals(players[0], game.players[0].getName());
+  }
+}
