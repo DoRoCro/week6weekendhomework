5450e2d652848660b4bcd93f0552d882a8a0d009
Douglas Crooke <douglas.crooke+codeclan@googlemail.com>
2017-03-12 11:05:48 +0000

    trying to get a TurnLog to work...

--- cardgame/TurnLog.java
+++ cardgame/TurnLog.java
@@ -16,9 +16,18 @@ public class TurnLog{
     return new Hand(player1);
   }
 
-  public void updateLogHistory(HashMap entry){
-    log.add(entry);
+  // public Player getPlayer(){
+  //   return this.logentry.keySet().toArray()[0];
+  // }
+
+  public void addToLog(){
+    log.add(this.logentry);
   }
+
+  // public static TurnLog getLastLog(){
+  //   HashMap<Player,Hand> lastlog = log.get(log.size()-1);
+  //   return new TurnLog( lastlog.keySet().toArray()[0], lastlog.values().toArray()[0]);
+  // }
 
 
 
--- specs/TurnLogTest.java
+++ specs/TurnLogTest.java
@@ -11,14 +11,20 @@ public class TurnLogTest{
   String[] players;
   TurnLog logmesg ;
   Card card1;
+  Card card2;
   Hand hand1;
+  Hand hand2;
 
   @Before
   public void before(){
     player1 = new Player(player1name);
     player2 = new Player(player2name);
     card1 = new Card(Suit.HEARTS, CardNumber.THREE);
+    card2 = new Card(Suit.HEARTS, CardNumber.FOUR);
     hand1 = new Hand(player1);
+    hand2 = new Hand(player2);
+    hand1.receiveCard(card1);
+    hand2.receiveCard(card2);
   }
 
   @Test
@@ -27,5 +33,21 @@ public class TurnLogTest{
     assertEquals(hand1.getName(), logmesg.getHand(player1).getName() );
   }
 
+  // @Test public void canGetPlayerFromTurnLog() {
+  //   logmesg = new TurnLog(player1, hand1);
+  //   assertEquals(player1.getName(), logmesg.getPlayer().getName())
+
+
+  // }
+  // @Test
+  // public void canGetTurnLogBack() {
+  //   logmesg = new TurnLog(player1, hand1);
+  //   logmesg.addToLog();
+  //   logmesg = new TurnLog(player2, hand2);
+  //   logmesg.addToLog();
+  //   TurnLog retrieved = TurnLog.getLastLog();
+  //   assertEquals(logmesg.getHand(player2).getName(), retrieved.getHand(player2).getName());
+
+  // }
 
 }
